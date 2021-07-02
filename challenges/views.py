25383b913c0f0ec28829f149f705b7f2964df7cc
corneliu12 <42445417+corneliu12@users.noreply.github.com>
2021-07-01 21:01:40 -0500

    created dynamic path segment for monthly challenges with values and remove the hard code from previous commits

--- challenges/views.py
+++ challenges/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
 
-def january(request):
-    return HttpResponse("Eat meat everyday for the entire month!")
+def monthly_challenge(request, month):
+    challenge_text = None
+    if month == "january":
+        challenge_text = "Eat meat everyday for the entire month!"
+    elif month == 'february':
+        challenge_text = "Walk for at least 20 minutes a day!"
+    elif month == "march":
+        challenge_text = "Learn Django for 20 min a day!"
+    else:
+        return HttpResponseNotFound("This month is not supported!")
 
-def february(request):
-    return HttpResponse("Walk for at least 20 minutes a day!")
+    return HttpResponse(challenge_text)
